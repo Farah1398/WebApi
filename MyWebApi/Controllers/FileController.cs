d769209713a4a8ebeef24e02dd975aff5b890872
Farah1398 <farah.ayari@esprit.tn>
2022-10-03 00:48:14 +0100

    controllers

---
+++ MyWebApi/Controllers/FileController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyWebApi.Data;
+using MyWebApi.Models;
+
+namespace MyWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        //use the dependency injection to access the config. from "appsettings.cs"
+        private readonly DataContext _context;
+        public FileController(DataContext context)
+        {
+            _context = context;
+        }
+
+
+
+
+        // GET: api/File
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Table_File>>> Getfiles()
+        {
+            return await _context.files.ToListAsync();
+        }
+
+        // GET: api/File/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Table_File>> GetTable_File(int id)
+        {
+            var table_File = await _context.files.FindAsync(id);
+
+            if (table_File == null)
+            {
+                return NotFound();
+            }
+
+            return table_File;
+        }
+
+        // PUT: api/File/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTable_File(int id, Table_File table_File)
+        {
+            if (id != table_File.FileId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(table_File).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!Table_FileExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/File
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Table_File>> PostTable_File(Table_File table_File)
+        {
+            _context.files.Add(table_File);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTable_File", new { id = table_File.FileId }, table_File);
+        }
+
+        // DELETE: api/File/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTable_File(int id)
+        {
+            var table_File = await _context.files.FindAsync(id);
+            if (table_File == null)
+            {
+                return NotFound();
+            }
+
+            _context.files.Remove(table_File);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool Table_FileExists(int id)
+        {
+            return _context.files.Any(e => e.FileId == id);
+        }
+    }
+}
---
+++ MyWebApi/Controllers/ServersController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyWebApi.Data;
+using MyWebApi.Models;
+
+namespace MyWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ServersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Servers
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Server>>> Getservers()
+        {
+            return await _context.servers.ToListAsync();
+        }
+
+        // GET: api/Servers/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Server>> GetServer(int id)
+        {
+            var server = await _context.servers.FindAsync(id);
+
+            if (server == null)
+            {
+                return NotFound();
+            }
+
+            return server;
+        }
+
+        // PUT: api/Servers/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutServer(int id, Server server)
+        {
+            if (id != server.ServerId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(server).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ServerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Servers
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Server>> PostServer(Server server)
+        {
+            _context.servers.Add(server);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetServer", new { id = server.ServerId }, server);
+        }
+
+        // DELETE: api/Servers/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteServer(int id)
+        {
+            var server = await _context.servers.FindAsync(id);
+            if (server == null)
+            {
+                return NotFound();
+            }
+
+            _context.servers.Remove(server);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ServerExists(int id)
+        {
+            return _context.servers.Any(e => e.ServerId == id);
+        }
+    }
+}
